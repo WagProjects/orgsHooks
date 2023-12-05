c078a486e8ea52ec774440dde91e8e6321a0d7f0
WagProjects <contato-wagneros@outlook.com>
2023-12-05 13:18:05 -0300

    init

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+function app(){
+  return (
+    <SafeAreaView>
+      <Text>Alura</Text>
+    </SafeAreaView>
+  )
+}
+
+export default app
